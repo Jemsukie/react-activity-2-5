76240f05c3afa23b6617a449a0d92c78d789d230
Jemsukie <jemuel.lupo@gmail.com>
2022-07-05 00:04:47 +0800

    Made a JSX file with a Navbar Component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () =>{
+
+    return (
+        <>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="container">
+                    <a className="navbar-brand">Home</a>
+                </div>
+                
+            </nav>
+        </>
+    )
+}
+export default Nav;
