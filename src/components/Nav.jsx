1e878dee2ed5b8d9779fff9db1c662a7e9f33471
Jemsukie <jemuel.lupo@gmail.com>
2022-07-05 01:15:22 +0800

    Understnad React lifecycle by implementing a clock that ticks every second

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
+import Clock from "./Clock";
+
 const Nav = () =>{
+
+    const date = new Date();
 
     return (
         <>
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container">
                     <a className="navbar-brand">Home</a>
+                    <div className="text-white"><Clock date = {date}/></div>
                 </div>
                 
             </nav>
